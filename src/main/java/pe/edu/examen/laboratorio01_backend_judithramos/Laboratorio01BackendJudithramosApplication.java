0adb2737ef8a877d221e62308ca86db3010b4291
Judith14D <Judith.ramos.supanta@gmail.com>
2024-09-22 19:43:36 -0500

    Carga inicial del proyecto laboratorio

---
+++ src/main/java/pe/edu/examen/laboratorio01_backend_judithramos/Laboratorio01BackendJudithramosApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.examen.laboratorio01_backend_judithramos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Laboratorio01BackendJudithramosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Laboratorio01BackendJudithramosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/examen/laboratorio01_backend_judithramos/Laboratorio01BackendJudithramosApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.examen.laboratorio01_backend_judithramos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Laboratorio01BackendJudithramosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
